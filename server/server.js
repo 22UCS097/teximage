94f6b615c2691fe14c40025e313d992bd6c1a3e7
22UCS097 <ravikumar628763@gmail.com>
2025-03-31 14:52:19 +0530

    user Schema created

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 import express from "express"
 import cors from 'cors'
 import 'dotenv/config'
-import connectDB from "./config/mogodb.js"
+import connectDB from "./config/mongodb.js"
 
 const PORT=process.env.PORT || 4000
 const app =express();
@@ -9,12 +9,9 @@ const app =express();
 app.use(express.json())
 app.use(cors());
 
-await connectDB();
+ await connectDB();
 
 
 app.get('/',(req,res)=> res.send("API working"));
 
 app.listen(PORT,()=> console.log("server started",PORT));
-
-
-
